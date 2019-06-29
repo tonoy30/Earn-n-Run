94e2c07380f425286924edbac64e8b49942a71e7
Ali Ahmmed Tonoy <tonoy.sust@yahoo.com>
2019-06-29 15:58:01 +0600

    init

---
+++ core/src/com/tonoy/eat_run/GameMain.java
@@ -0,0 +1,29 @@
+package com.tonoy.eat_run;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameMain extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture img;
+    Texture img2;
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
